672084055ebbf8729e3887d5fb4f029e7a371b81
Masrafi Bin Mobarak Badhon <144796398+MBM-Badhon@users.noreply.github.com>
2025-04-19 14:58:53 +0600

    Update gui_factory.cpp
    Deleted some functions to experiment commitement

--- gui_factory.cpp
+++ gui_factory.cpp
@@ -18,20 +18,5 @@ std::string LinuxScrollbar::render() {
     return "Rendering Linux scrollbar";
 }
 
-// Windows factory
-std::unique_ptr<Window> WindowsFactory::createWindow() {
-    return std::make_unique<WindowsWindow>();
-}
 
-std::unique_ptr<Scrollbar> WindowsFactory::createScrollbar() {
-    return std::make_unique<WindowsScrollbar>();
-}
-
-// Linux factory
-std::unique_ptr<Window> LinuxFactory::createWindow() {
-    return std::make_unique<LinuxWindow>();
-}
-
-std::unique_ptr<Scrollbar> LinuxFactory::createScrollbar() {
-    return std::make_unique<LinuxScrollbar>();
 }
